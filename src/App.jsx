88687ac752a3f8293f819a319836711cb68c7f45
Noobmaster69-lang <140466782+Noobmaster69-lang@users.noreply.github.com>
2023-11-03 18:11:36 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import './App.css'
+import FormUser from './components/FormUser'
+import UserCard from './components/UserCard'
+import useFetch from './hooks/useFetch'
+
+function App() {
+
+  const [infoUpdate, setInfoUpdate] = useState()
+  const [estilo, setEstilo] = useState('oculto')
+  const [estilo2, setEstilo2] = useState('oculto2')
+  const [nameDelete, setNameDelete] = useState('unknown')
+  const { register, handleSubmit, reset } = useForm()
+
+
+  const baseUrl = 'http://localhost:8080'
+  const [users, getUsers, createUser, deleteUser, updateUser] = useFetch(baseUrl)
+
+  useEffect(() => {
+    getUsers('/User')
+  }, [])
+
+
+  const handleModalForm = () => {
+    setInfoUpdate()
+    reset({
+      first_name: '', 
+      last_name: '',
+      email: '',
+      password: '',
+      birthday: ''
+    })
+    if (estilo === 'mostrar') {
+      setEstilo('oculto')
+    } else {
+      setEstilo('mostrar')
+    }
+  }
+
+  const handleModalForm2 = () => {
+    if (estilo === 'mostrar') {
+      setEstilo('oculto')
+    } else {
+      setEstilo('mostrar')
+    }
+  }
+
+  const handleModalDelete = () => {
+    if (estilo2 === 'mostrar2') {
+      setEstilo2('oculto2')
+    } else {
+      setEstilo2('mostrar2')
+    }
+  }
+
+  return (
+    <div className='principal'>
+      <header className="header">
+        <h1 className='header__title'>Users</h1>
+        <button className='header__btn-create' onClick={handleModalForm}>
+          <i className='bx bx-plus'></i>
+          Create new user
+        </button>
+      </header>
+      <FormUser
+        createUser={createUser}
+        infoUpdate={infoUpdate}
+        updateUser={updateUser}
+        setInfoUpdate={setInfoUpdate}
+        estilo={estilo}
+        handleModalForm2={handleModalForm2}
+        handleModalForm={handleModalForm}
+        register={register}
+        handleSubmit={handleSubmit}
+        reset={reset}
+      />
+      <div className='container-users'>
+        <div className={`${estilo2} modal-accept`}>
+          <div className="accept__container">
+            <h2 className='accept__tittle'>User delete</h2>
+            <p className='accept__text'>User <span className='accept__name'>{nameDelete}</span>  has been delete </p>
+            <button className='accept__btn' onClick={handleModalDelete}>Confirm</button>
+          </div>
+        </div>
+        {
+          users?.map(user => (
+            <UserCard
+              key={user.id}
+              user={user}
+              deleteUser={deleteUser}
+              setInfoUpdate={setInfoUpdate}
+              setNameDelete={setNameDelete}
+              handleModalDelete={handleModalDelete}
+              handleModalForm2={handleModalForm2}
+            />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FormUser.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import '../assets/styles/FormUser.css'
+
+const FormUser = ({ createUser,infoUpdate,updateUser,setInfoUpdate,estilo,handleModalForm,handleModalForm2,register,handleSubmit,reset}) => {
+
+    useEffect(() => {
+        reset(infoUpdate)
+    }, [infoUpdate])
+
+
+    const submit = data => {
+        if (infoUpdate) {
+            updateUser('/User', infoUpdate.id, data)
+            setInfoUpdate()
+            handleModalForm2()
+        } else {
+            createUser('/User', data)
+            handleModalForm2()
+        }
+        reset({
+            email: '',
+            password: '',
+            first_name: '',
+            last_name: '',
+            birthday: ''
+        })
+    }
+
+
+
+
+    return (
+        <div className={`${estilo} container-form`} >
+            <form onSubmit={handleSubmit(submit)} className='form'>
+                <div className="form-header">
+                    <h2 className='form__tittle'>{infoUpdate ? 'Edit user' : 'New user'}</h2>
+                    <span className='btn-close' onClick={handleModalForm}>
+                        <i className='bx bx-x'></i>
+                    </span>
+                </div>
+                <div className='form__date'>
+                    <label htmlFor="email">Email</label>
+                    <input className='input-form' {...register('email')} type="email" id="email" />
+                </div>
+                <div className='form__date'>
+                    <label htmlFor="password">Password</label>
+                    <input className='input-form' {...register('password')} type="password" id="password" />
+                </div>
+                <div className='form__date'>
+                    <label htmlFor="first_name">First name</label>
+                    <input className='input-form' {...register('first_name')} type="text" id="first_name" />
+                </div>
+                <div className='form__date'>
+                    <label htmlFor="last_name">Last name</label>
+                    <input className='input-form' {...register('last_name')} type="text" id="last_name" />
+                </div>
+                <div className='form__date'>
+                    <label htmlFor="birthday">Birthday</label>
+                    <input className='input-form' {...register('birthday')} type="number" id="birthday" />
+                </div>
+                <button className='form__button'>{infoUpdate ? 'Guardar Cambios' : 'Agregar nuevo usuario'}</button>
+            </form>
+        </div>
+    )
+}
+
+export default FormUser
